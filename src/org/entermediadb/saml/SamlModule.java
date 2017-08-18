d45c100d2fb31b875d246f85a9f9ae81542e740f
netevolved <ian@netevolved.com>
2017-08-18 15:46:23 -0400

    Getting closer.

--- src/org/entermediadb/saml/SamlModule.java
+++ src/org/entermediadb/saml/SamlModule.java
@@ -1,26 +1,58 @@
 package org.entermediadb.saml;
 
+import java.util.List;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.entermediadb.asset.modules.BaseMediaModule;
 import org.openedit.WebPageRequest;
+import org.openedit.page.manage.PageManager;
 
 import com.onelogin.saml2.Auth;
 
 public class SamlModule extends BaseMediaModule
 {
-
+	protected PageManager fieldPageManager;
+	
 	
 	public void startAuthentication(WebPageRequest inReq) throws Exception{
 		HttpServletRequest request = inReq.getRequest();
-		Auth auth = new Auth(request, inReq.getResponse());
-		if (request.getParameter("attrs") == null) {
-			auth.login();
-		} else {
+		
+		
+		
+	
+		Auth auth = 	new Auth( request, inReq.getResponse());
+		
 			String x = request.getPathInfo();
-			auth.login("/java-saml-tookit-jspsample/attrs.jsp");
-		}
+			auth.login("/saml/consume.html");
+						
+
 	}
+	
+	
+	public void processResponse(WebPageRequest inReq)  throws Exception{
+		
+		
+		Auth auth = 	new Auth( inReq.getRequest(), inReq.getResponse());
+		
+		auth.processResponse();
+		if(auth.isAuthenticated()){
+			Map<String, List<String>> attributes = auth.getAttributes();
+			String metadata = auth.getSettings().getSPMetadata();
+
+			
+		}
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
 	
 	
 }
