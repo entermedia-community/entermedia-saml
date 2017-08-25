f97fc835eb09a4ab6478efd1b6a922cdd3b38d07
Ian <ian@netevolved.com>
2017-08-25 12:42:09 -0400

    New code

--- src/org/entermediadb/saml/SamlModule.java
+++ src/org/entermediadb/saml/SamlModule.java
@@ -7,10 +7,13 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.entermediadb.asset.importer.BaseHotFolderManager;
+import org.entermediadb.asset.MediaArchive;
 import org.entermediadb.asset.modules.BaseMediaModule;
 import org.openedit.WebPageRequest;
+import org.openedit.event.WebEvent;
 import org.openedit.page.manage.PageManager;
+import org.openedit.users.User;
+import org.openedit.users.UserSearcher;
 
 import com.onelogin.saml2.Auth;
 import com.onelogin.saml2.settings.Saml2Settings;
@@ -61,7 +64,42 @@ public class SamlModule extends BaseMediaModule
 			log.info(auth.getLastErrorReason());
 
 		}
-		
+		MediaArchive archive = getMediaArchive(inReq);
+		UserSearcher searcher = (UserSearcher) archive.getSearcher("user");
+//		User target = searcher.getUserByEmail(email);
+//		if (target == null)
+//		{
+//			target = (User) searcher.createNewData();
+//			target.setFirstName(firstname);
+//			target.setLastName(lastname);
+//			target.setEmail(email);
+//			target.setEnabled(true);
+//			searcher.saveData(target, null);			
+//		}
+//		if(target != null){
+//			
+//			inReq.putSessionValue(searcher.getCatalogId() + "user", target);
+//			String md5 = getCookieEncryption().getPasswordMd5(target.getPassword());
+//			String value = target.getUserName() + "md542" + md5;
+//			inReq.putPageValue("entermediakey", value);
+//			inReq.putPageValue( "user", target);
+//			if(getEventManager() != null)
+//			{
+//				WebEvent event = new WebEvent();
+//				event.setSearchType("userprofile");
+//				event.setCatalogId(searcher.getCatalogId());
+//				event.setOperation("saved");
+//				event.setProperty("dataid", target.getId());
+//				event.setProperty("id", target.getId());
+//
+//				event.setProperty("applicationid", inReq.findValue("applicationid"));
+//
+//				getEventManager().fireEvent(event);
+//			}
+//
+//
+//		}
+
 		
 		
 		
